52785429ab370da19d7660bfd6d394873707ea53
Patrick Lowry <patio.lowry@gmail.com>
2026-05-29 16:55:41 -0400

    scaffolding for create link endpoint

---
+++ app/api/routes/links.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+from pydantic import BaseModel, AnyUrl
+
+router = APIRouter()
+
+class NewLinkResponse(BaseModel):
+    fullUrl: AnyUrl
+    shortUrl: AnyUrl
+
+class Url(BaseModel):
+    fullUrl: AnyUrl
+
+@router.post("/", response_model=NewLinkResponse)
+def create_short_url(url: Url):
+    return { "fullUrl": url.fullUrl, "shortUrl": "http://du.mmy/123456"}
--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from app.api.routes import ping
+from app.api.routes import ping, links
 
 app = FastAPI(title="Link Shortener")
 
 app.include_router(ping.router, prefix="/ping", tags=["ping"])
+app.include_router(links.router, prefix="/links", tags=["links"])
