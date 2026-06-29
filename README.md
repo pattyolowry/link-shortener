f48ec2eab0061e5a5767990c5c6c89275c134c57
Patrick Lowry <patio.lowry@gmail.com>
2026-06-29 12:34:13 -0700

    endpoint to redirect short urls

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # link-shortener
+
+start with: `docker compose up --build`
+
+down with : `docker compose down`
--- app/api/routes/links.py
+++ app/api/routes/links.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from fastapi.responses import RedirectResponse
 from pydantic import BaseModel, AnyUrl
 from ...services.clients import short_id_generator
 
@@ -15,3 +16,7 @@ class Url(BaseModel):
 async def create_short_url(url: Url):
     short_id = await short_id_generator.get_new_id()
     return { "fullUrl": url.fullUrl, "shortUrl": f"http://du.mmy/{short_id}"}
+
+@router.get("/{short_id}", response_class=RedirectResponse, status_code=302)
+async def redirect_short_url(short_id: str):
+    return "https://www.wikipedia.org/"  
